c1f88234398b539404e4cbb979076b1ae78bd4cf
J. Mohr <pixels.and.glitches@gmail.com>
2021-05-29 19:13:24 -0500

    Made Break and Session components uniform

--- src/components/Break.js
+++ src/components/Break.js
@@ -4,18 +4,18 @@ class Break extends Component {
   render() {
     const { breakLength, increment, decrement, timerRunning } = this.props;
     return (
-      <div className='break-buttons buttons'>
+      <div className="break-buttons buttons">
         <button
-          className='btn btn-left'
-          onClick={() => decrement('break')}
+          className="btn btn-left"
+          onClick={() => decrement()}
           disabled={timerRunning}
         >
           -
         </button>
-        <p className='interval-length'>{breakLength}</p>
+        <p className="interval-length">{breakLength}</p>
         <button
-          className='btn btn-right'
-          onClick={() => increment('break')}
+          className="btn btn-right"
+          onClick={() => increment()}
           disabled={timerRunning}
         >
           +
--- src/components/Session.js
+++ src/components/Session.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react';
 
 class Session extends Component {
-  constructor(props) {
-    super(props);
-    this.handleDecrement = this.handleDecrement.bind(this);
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleDecrement() {
-    this.props.decrement('session');
-  }
-
-  handleIncrement() {
-    this.props.increment('session');
-  }
-
   render() {
-    let { sessionLength, timerRunning } = this.props;
+    const { sessionLength, increment, decrement, timerRunning } = this.props;
     return (
-      <div className='session-buttons buttons'>
+      <div className="session-buttons buttons">
         <button
-          className='btn btn-left'
-          onClick={this.handleDecrement}
-          enabled={!timerRunning}
+          className="btn btn-left"
+          onClick={() => decrement()}
           disabled={timerRunning}
         >
           -
         </button>
-        <p className='interval-length'>{sessionLength}</p>
+        <p className="interval-length">{sessionLength}</p>
         <button
-          className='btn btn-right'
-          onClick={this.handleIncrement}
+          className="btn btn-right"
+          onClick={() => increment()}
           disabled={timerRunning}
         >
           +
